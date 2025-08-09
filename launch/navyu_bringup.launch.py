a816118045b989b4959450412f6c2c865411fa64
RyuYamamoto <ryu.yamamoto@tier4.jp>
2025-08-09 21:31:16 +0900

    update (#1)

--- launch/navyu_bringup.launch.py
+++ launch/navyu_bringup.launch.py
@@ -27,16 +27,8 @@ def generate_launch_description():
 
     rviz_config = PathJoinSubstitution([FindPackageShare("kachaka_navyu"), "rviz", "kachaka_navyu.rviz"])
 
-    costmap_map_config_path = PathJoinSubstitution(
-        [FindPackageShare("kachaka_navyu"), "config", "navyu_costmap_2d_params.yaml"]
-    )
-
-    navyu_global_planner_config = PathJoinSubstitution(
-        [FindPackageShare("kachaka_navyu"), "config", "navyu_global_planner_params.yaml"]
-    )
-
-    navyu_path_tracker_config = PathJoinSubstitution(
-        [FindPackageShare("kachaka_navyu"), "config", "navyu_path_tracker_params.yaml"]
+    navyu_config_path = PathJoinSubstitution(
+        [FindPackageShare("kachaka_navyu"), "config", "navyu_params.yaml"]
     )
 
     return LaunchDescription(
@@ -47,23 +39,37 @@ def generate_launch_description():
             Node(
                 package="navyu_costmap_2d",
                 executable="navyu_costmap_2d_node",
-                name="global_costmap_node",
-                remappings=[("/map", "/kachaka/mapping/map")],
-                parameters=[costmap_map_config_path, {"use_sim_time": use_sim_time}],
+                name="navyu_global_costmap_node",
+                remappings=[("/map", "/kachaka/mapping/map"), ("/costmap", "/global_costmap")],
+                parameters=[navyu_config_path, {"use_sim_time": use_sim_time}],
+            ),
+            Node(
+                package="navyu_costmap_2d",
+                executable="navyu_costmap_2d_node",
+                name="navyu_local_costmap_node",
+                remappings=[("/costmap", "/local_costmap")],
+                parameters=[navyu_config_path, {"use_sim_time": use_sim_time}],
             ),
             Node(
                 package="navyu_path_planner",
                 executable="navyu_global_planner_node",
                 name="navyu_global_planner_node",
-                remappings=[("/goal_pose", "/goal_pose_1")],
-                parameters=[navyu_global_planner_config, {"use_sim_time": use_sim_time}],
+                remappings=[("/goal_pose", "/goal_pose_1"), ("/costmap", "/global_costmap")],
+                parameters=[navyu_config_path, {"use_sim_time": use_sim_time}],
             ),
             Node(
                 package="navyu_path_tracker",
                 executable="navyu_path_tracker_node",
                 name="navyu_path_tracker_node",
-                remappings=[("/cmd_vel", "/kachaka/manual_control/cmd_vel")],
-                parameters=[navyu_path_tracker_config, {"use_sim_time": use_sim_time}],
+                remappings=[("/cmd_vel", "cmd_vel_in")],
+                parameters=[navyu_config_path, {"use_sim_time": use_sim_time}],
+            ),
+            Node(
+                package="navyu_safety_limiter",
+                executable="navyu_safety_limiter_node",
+                name="navyu_safety_limiter_node",
+                remappings=[("/cmd_vel_out", "/kachaka/manual_control/cmd_vel")],
+                parameters=[navyu_config_path, {"use_sim_time": use_sim_time}],
             ),
             Node(
                 package="rviz2",
